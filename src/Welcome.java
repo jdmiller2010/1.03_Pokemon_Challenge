585de01d06aadbbd586e192654ecd8886ab04fcc
Jordan Miller <jdmiller2010@gmail.com>
2018-09-06 21:45:24 -0700

    added src back in

---
+++ src/Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println ("Pikachu welcomes you to the world of Pokémon!\n");
+		System.out.println ("(\\_/)");
+		System.out.println ("(o^.^)");
+		System.out.println ("z(_(\")(\")");
+
+	}
+
+}
