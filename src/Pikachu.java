81bf47cc70618f4617fe450609808436e3e374a6
Jordan Miller <jdmiller2010@gmail.com>
2018-09-06 21:27:15 -0700

    Part way done

--- src/Pikachu.java
+++ src/Pikachu.java
@@ -2,7 +2,9 @@
 public class Pikachu {
 
 	public static void main(String[] args) {
-		System.out.println ("Hello");
+		System.out.println ("Pikachu welcomes you to the world of Pokémon!\n");
+		System.out.println ("(\\_/)");
+		
 
 	}
 
